d761266cb256e5b54f8f3e35837464fc6e322476
Shashank Aditya <msrsaditya@gmail.com>
2023-10-21 21:37:07 +0530

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- Line Numbers
+vim.cmd('set number')
+
+-- Clipboard
+vim.cmd('set clipboard=unnamedplus')
+
+-- Mouse Support
+vim.cmd('set mouse=a')
+
+-- Indentation settings
+vim.cmd('set autoindent')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
+vim.cmd('set expandtab')
