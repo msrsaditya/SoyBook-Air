90564f1b73ab93c1c4770ec2437e68dd587c865c
Shashank Aditya <msrsaditya@gmail.com>
2025-01-29 02:12:08 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,27 @@
--- Line Numbers
-vim.cmd('set number')
+-- Basics
+vim.opt.number = true              -- Show line numbers
+vim.opt.mouse = 'a'                -- Enable mouse support
+vim.opt.clipboard = 'unnamedplus'  -- Use system clipboard
 
--- Clipboard
-vim.cmd('set clipboard=unnamedplus')
+-- Search
+vim.opt.ignorecase = true     -- Case insensitive search
+vim.opt.hlsearch = true       -- Highlight search results
+vim.opt.incsearch = true      -- Show search matches as you type
 
--- Mouse Support
-vim.cmd('set mouse=a')
+-- Indentation
+vim.opt.expandtab = true      -- Convert tabs to spaces
+vim.opt.shiftwidth = 4        -- Number of spaces for auto-indent
+vim.opt.tabstop = 4           -- Number of spaces that a tab counts for
+vim.opt.smartindent = true    -- Enable smart-indent
 
--- Indentation settings
-vim.cmd('set autoindent')
-vim.cmd('set tabstop=4')
-vim.cmd('set shiftwidth=4')
-vim.cmd('set expandtab')
+-- Editor
+vim.opt.wrap = false          -- Don't wrap lines
+
+-- File
+vim.opt.fileencoding = 'utf-8' -- Default file encoding
+vim.opt.backup = false         -- Don't create backup files
+vim.opt.swapfile = false       -- Don't create swap files
+vim.opt.undofile = true        -- Enable persistent undo
+
+-- Visual
+vim.opt.termguicolors = true  -- Enable 24-bit RGB colors
